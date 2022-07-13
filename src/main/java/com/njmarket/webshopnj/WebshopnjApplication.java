91a720d95113898953cc522ff965997ae31b0dc7
seungwan-jung <saddler-tenure0i@icloud.com>
2022-07-13 15:13:27 +0900

    Initial commit

---
+++ src/main/java/com/njmarket/webshopnj/WebshopnjApplication.java
@@ -0,0 +1,13 @@
+package com.njmarket.webshopnj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopnjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopnjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/njmarket/webshopnj/WebshopnjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njmarket.webshopnj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopnjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
