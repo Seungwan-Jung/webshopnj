e9f16508b192231766a2a577ac29628d8bd0c9c3
seungwan-jung <saddler-tenure0i@icloud.com>
2022-09-15 23:05:05 +0900

    file upload

--- src/main/java/com/njmarket/webshopnj/controller/PostController.java
+++ src/main/java/com/njmarket/webshopnj/controller/PostController.java
@@ -7,18 +7,29 @@ import com.njmarket.webshopnj.dto.PostSearchCondition;
 import com.njmarket.webshopnj.service.PostService;
 import com.njmarket.webshopnj.web.PostForm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class PostController {
     private final PostService postService;
+
+    @Value("${file.dir}")
+    private String fileDir;
 
     @GetMapping(value = "/posts/new")
     public String createForm(Model model) {
@@ -28,7 +39,7 @@ public class PostController {
     }
 
     @PostMapping(value = "/posts/new")
-    public String create(PostForm form) {
+    public String create(PostForm form, @RequestParam MultipartFile file, HttpServletRequest request) throws IOException {
 
         Post post = new Post();
         post.setTitle(form.getTitle());
@@ -37,6 +48,15 @@ public class PostController {
         post.setDetail(form.getDetail());
 
         postService.savePost(post);
+
+        log.info("request={}",request);
+        log.info("multipartFile={}", file);
+
+        if (!file.isEmpty()) {
+            String fullPath = fileDir + file.getOriginalFilename();
+            log.info("file save fullPath={}", fullPath);
+            file.transferTo(new File(fullPath));
+        }
         return "redirect:/posts";
     }
 
